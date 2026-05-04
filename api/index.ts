71cf1bccd4582a71d5ef5c3881ca4e9225a2d7b3
lea-bensoussan-cosmo5 <lea.bensoussan@cosmo5.com>
2026-05-04 09:58:43 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
