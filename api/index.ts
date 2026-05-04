8f8ce8cf27afd2f2b22af564b16693a01f294927
lea-bensoussan-cosmo5 <lea.bensoussan@cosmo5.com>
2026-05-04 10:14:48 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
