a75d8e5e9a584b91b70378d1706ce84871a5fd26
lea-bensoussan-cosmo5 <lea.bensoussan@cosmo5.com>
2026-05-04 09:56:18 +0200

    Update callback.ts

--- api/auth/callback.ts
+++ api/auth/callback.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { UserAuthManager, UserSession } from "../../src/utils/user-auth.js";
 
 export default async function handler(
